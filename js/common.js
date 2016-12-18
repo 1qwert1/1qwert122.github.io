f0bc03b9a4a4f69f3daefd9a58f2d258d84d23da
nemo <vzabudko@gmail.com>
2016-12-18 17:12:57 +0200

    hjhjh

--- js/common.js
+++ js/common.js
@@ -12,11 +12,22 @@ $(function() {
             function(){ $(this).addClass('active-top') },
             function(){ $(this).removeClass('active-top') }
 		)
+
+
     var heightLogo = $('#logo').outerHeight();
     $(window).scroll(function () {
         if ($(this).scrollTop() >= heightLogo) {
             $('.header_top').addClass('stickytop');
+            if (!$('*').is('#newDiv')) {
+                $('ul.rslides').before(function () {
+                    return "<div id='newDiv' style='height: "+$('.header_top').outerHeight()+ "px' ></div>"
+
+                });
+            }
+
+
         } else {
+            $('#newDiv').remove();
             $('.header_top').removeClass('stickytop');
         }
     });
